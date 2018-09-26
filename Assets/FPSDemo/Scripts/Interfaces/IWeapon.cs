6a47009fbe9d3a452a6668869520203b69213ff9
Дмитрий Соколовский <bewreks@mail.ru>
2018-09-26 12:24:29 +0300

    + Scripts

---
+++ Assets/FPSDemo/Scripts/Interfaces/IWeapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPSDemo
+{
+    public interface IWeapon
+    {
+        void Fire();
+        void Reload();
+        bool IsActive();
+        GameObject GameObject { get; }
+    }
+}
---
+++ Assets/FPSDemo/Scripts/Models/PlayerModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPSDemo
+{
+    public class PlayerModel : BaseModel
+    {
+        public List<IWeapon> Weapons;
+        public int CurrentWeapon = 0;
+
+        public float SwitchWeaponTimeout = 0.5f;
+        public float LastSwitchWeapon;
+
+        public IWeapon CurrentWeaponController => Weapons[CurrentWeapon];
+        public bool IsTimeout => Time.time < LastSwitchWeapon + SwitchWeaponTimeout;
+        public int WeaponsCount => Weapons.Count - 1;
+    }
+}
