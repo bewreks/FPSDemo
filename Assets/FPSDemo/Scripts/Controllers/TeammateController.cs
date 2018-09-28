fa54a586b641b7ec6a165410617eadbca412e0bc
Дмитрий Соколовский <bewreks@mail.ru>
2018-09-28 22:42:50 +0300

    + ethan (model only) + door (temp script only)

---
+++ Assets/FPSDemo/Scripts/Controllers/TeammateController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPSDemo
+{
+    public class TeammateController : BaseController<TeammateModel>
+    {
+        protected override void Initialize()
+        {
+            
+        }
+    }
+}
