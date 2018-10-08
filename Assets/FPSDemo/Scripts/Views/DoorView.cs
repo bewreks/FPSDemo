5399ace1965f247076fcaf00bc79da44b885d9e3
bewreks <bewreks@mail.ru>
2018-10-09 00:40:57 +0400

    + Door animation. Contains bug

--- Assets/FPSDemo/Scripts/Views/DoorView.cs
+++ Assets/FPSDemo/Scripts/Views/DoorView.cs
@@ -6,12 +6,20 @@ namespace FPSDemo
 {
     public class DoorView : BaseView<DoorModel>
     {
+        public float TranslateTime = 1;
+        
         protected Transform Door;
+        public float _time;
+
+        private Vector3 _startPosition;
+        private Vector3 _destinition;
         
         protected override void Initialize()
         {
-            _model.OnSwitch += OnSwitch;
             Door = transform.GetChild(0);
+            _model.OnSwitch += OnSwitch;
+            _destinition = Door.transform.position;
+            _startPosition = _destinition + _model.Offset;
         }
 
         private void OnSwitch()
@@ -28,12 +36,38 @@ namespace FPSDemo
 
         private void Close()
         {
-            Door.Translate(-_model.Offset);
+            SetTime();
+            Swap();
         }
 
         private void Open()
         {
-            Door.Translate(_model.Offset);
+            SetTime();
+            Swap();
+        }
+
+        private void SetTime()
+        {
+            _time = 1 - _time;
+        }
+
+        private void Swap()
+        {
+            var temp = _destinition;
+            _destinition = _startPosition;
+            _startPosition = temp;
+        }
+
+        private void Update()
+        {
+            if (_time <= 0)
+            {
+                return;
+            }
+            _time -= (Time.deltaTime / TranslateTime);
+            var transformPosition = Vector3.Lerp(_startPosition, _destinition, 1 - _time);
+            Debug.Log($"{transformPosition} - {1 - _time}");
+            Door.transform.position = transformPosition;
         }
     }
 }
